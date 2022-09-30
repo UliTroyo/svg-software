e047ebdf8ebccc680a8355d7df084adb3fef556d
Uli Troyo <5466444+UliTroyo@users.noreply.github.com>
2022-09-30 18:26:42 -0400

    Add mousemove canvas.

--- src/lib/Canvas.svelte
+++ src/lib/Canvas.svelte
@@ -3,17 +3,40 @@
   import Guides from "$lib/Guides.svelte";
   let guides = true; // TODO: place elsewhere
 
-  /*
-  TODO: create state machine for special actions
-  taken on keydown. When <space>, cursor should
-  switch to grab; on mousedown, cursor should
-  switch to grabbing and canvas should move. On
-  release of <space>, regular operation resumes.
-  */
+  let workArea: HTMLDivElement;
+
+  // TODO: create state machine for special actions
+  // taken on keydown. When <space>, cursor should
+  // switch to grab; on mousedown, cursor should
+  // switch to grabbing and canvas should move. On
+  // release of <space>, regular operation resumes.
+
+  function grabCanvas(event: MouseEvent) {
+    workArea.style.setProperty("--cursor", "grabbing");
+    workArea.addEventListener("mousemove", moveCanvas);
+    workArea.addEventListener("mouseup", dropCanvas);
+  }
+  function moveCanvas(event: MouseEvent) {
+    const { movementX, movementY } = event;
+    const left = Number(
+      getComputedStyle(workArea).getPropertyValue("--left").replace("px", "")
+    );
+    const top = Number(
+      getComputedStyle(workArea).getPropertyValue("--top").replace("px", "")
+    );
+    workArea.style.setProperty("--left", `${left + movementX}px`);
+    workArea.style.setProperty("--top", `${top + movementY}px`);
+  }
+
+  function dropCanvas(event: MouseEvent) {
+    workArea.style.setProperty("--cursor", "grab");
+    workArea.removeEventListener("mousemove", moveCanvas);
+    workArea.removeEventListener("mouseup", dropCanvas);
+  }
 </script>
 
 <div id="canvas">
-  <div class="work-area">
+  <div class="work-area" on:mousedown={grabCanvas} bind:this={workArea}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       data-shadow
